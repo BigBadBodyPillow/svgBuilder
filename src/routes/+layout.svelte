b8c322a914edb152a06003a1121f80296e0d45f4
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-09-21 18:42:44 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import '../reset.css';
+  import favicon from '$lib/assets/favicon.svg';
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+
+  <title>SVG Builder</title>
+</svelte:head>
+
+{@render children()}
